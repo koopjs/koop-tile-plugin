755cc3ed17384311fc04fd6db827dd5ab62c2fa7
Nate Goldman <nate@ngoldman.me>
2015-07-28 16:25:00 -0700

    :sparkles: standardize

--- index.js
+++ index.js
@@ -1,106 +1,94 @@
-var mkdirp = require('mkdirp'),
-  path = require('path'),
-  tiles = require('mapnik-tiles'),
-  fs = require('fs');
-
+var mkdirp = require('mkdirp')
+var tiles = require('mapnik-tiles')
+var fs = require('fs')
 
 // tells koop that this is plugin
-exports.type = 'plugin';
+exports.type = 'plugin'
 
 // name is used by koop to give models/controller access to plugin
-exports.name = 'tiles';
+exports.name = 'tiles'
 
-
-var getTile = function(params, data, callback ){
-  var x = parseInt( params.x ),
-    y = parseInt( params.y ),
-    z = parseInt( params.z ),
-    key = params.key,
-    format = params.format;
+function getTile (params, data, callback) {
+  var x = parseInt(params.x, 10)
+  var y = parseInt(params.y, 10)
+  var z = parseInt(params.z, 10)
+  var key = params.key
+  var format = params.format
 
   var options = {
     name: params.name,
     dir: params.dir
-  };
-
-  if (!params.x || !params.y || !params.z || !format || !key){
-    callback('Missing parameters', null);
-  } 
-  else {
-    // check the cache - the local file system 
-    checkFS( x, y, z, key, format, data, options, function( err, file ){
-      if ( file ){
-        callback( err, file );
-      } 
-      else {
-        callback( err, null );
-      }
-    });
   }
 
-};
-
-var checkFS = function( x, y, z, key, format, geojson, options, callback ){
-    var p = [ options.dir + 'tiles', key, format, z, x].join('/');
-    var file = p + '/' + y + '.' + format;
-
-    options.size = options.size || 256;
-
-    mkdirp( p, function(){
-      if ( !fs.existsSync( file ) ) {
-
-        var params = {
-          format: format,
-          name: options.name,
-          z: z,
-          x: x,
-          y: y
-        };
-
-        stashTile( file, geojson, params, function( err, newfile ){
-          callback( err, newfile );
-        });
-
+  if (!params.x || !params.y || !params.z || !format || !key) {
+    callback('Missing parameters', null)
+  } else {
+    // check the cache - the local file system
+    checkFS(x, y, z, key, format, data, options, function (err, file) {
+      if (file) {
+        callback(err, file)
       } else {
-        // file already exists 
-        callback( null, file );
+        callback(err, null)
       }
-    });
-  };
+    })
+  }
 
+}
 
-// calls mapnik-tiles to create a new tile 
+function checkFS (x, y, z, key, format, geojson, options, callback) {
+  var p = [options.dir + 'tiles', key, format, z, x].join('/')
+  var file = p + '/' + y + '.' + format
+
+  options.size = options.size || 256
+
+  mkdirp(p, function () {
+    if (!fs.existsSync(file)) {
+      var params = {
+        format: format,
+        name: options.name,
+        z: z,
+        x: x,
+        y: y
+      }
+
+      stashTile(file, geojson, params, function (err, newfile) {
+        callback(err, newfile)
+      })
+
+    } else {
+      // file already exists
+      callback(null, file)
+    }
+  })
+}
+
+// calls mapnik-tiles to create a new tile
 // for json it just writes to disk and returns
 // for png, pbf, or utf, it calls mapnik-tiles
-var stashTile = function( file, geojson, params, callback ){
-
+function stashTile (file, geojson, params, callback) {
   // for geojson tile just create it and return it
-  if ( params.format == 'json' ){
-
-    delete geojson.info;
-    delete geojson.name;
-    geojson.type = 'FeatureCollection';
-    fs.writeFile( file, JSON.stringify( geojson ), function(){
-      callback( null, file );
-    });
+  if (params.format === 'json') {
+    delete geojson.info
+    delete geojson.name
+    geojson.type = 'FeatureCollection'
+    fs.writeFile(file, JSON.stringify(geojson), function () {
+      callback(null, file)
+    })
 
   // for any other format call mapnik-tiles
   } else {
+    tiles.generate(geojson, params, function (err, tileBuffer) {
+      if (err) return callback(err, null)
 
-    tiles.generate( geojson, params, function( err, tileBuffer ){
-      if ( err ) {
-        callback( err, null );
-      } else {
-        fs.writeFile( file, tileBuffer, function( err ) {
-          callback( null, file );
-        });
-      }
-    });
-            
+      fs.writeFile(file, tileBuffer, function (err) {
+        if (err) return callback(err)
+        callback(null, file)
+      })
+    })
   }
-};
+}
 
-// expose the methods in the plugin 
-exports.stashTile = stashTile;
-exports.checkFS = checkFS;
-exports.getTile = getTile;
+// expose the methods in the plugin
+exports.stashTile = stashTile
+exports.checkFS = checkFS
+exports.getTile = getTile
--- test/tiles-test.js
+++ test/tiles-test.js
@@ -1,88 +1,84 @@
-var should = require('should');
-var fs = require('fs');
+/* global describe, it */
 
-var data = JSON.parse(fs.readFileSync(__dirname + '/fixtures/11.geojson').toString());
+var should = require('should')
+var fs = require('fs')
+
+var data = JSON.parse(fs.readFileSync(__dirname + '/fixtures/11.geojson', { encoding: 'utf8' }))
 var opts = {
   name: 'test-layer'
-};
-var tiles = require(__dirname + '/../');
+}
+var tiles = require(__dirname + '/../')
 
+describe('Tiles caching logic', function () {
+  describe('errors when params are wrong', function () {
+    it('when missing a z', function (done) {
+      tiles.getTile({x: 1, y: 1, format: 'png', key: 'fake-key'}, opts, function (err, res) {
+        should.exist(err)
+        should.not.exist(res)
+        done()
+      })
+    })
+  })
 
-describe('Tiles caching logic', function(){
+  describe('vector-tiles', function () {
+    it('when creating a tile', function (done) {
+      var file = __dirname + '/fixtures/co.6.13.24.vector.pbf'
 
+      var params = {
+        format: 'vector.pbf',
+        name: '61324',
+        z: 6,
+        x: 13,
+        y: 24
+      }
 
-    describe('errors when params are wrong', function(){
-      it('when missing a z', function(done){
-        tiles.getTile( {x: 1, y: 1, format: 'png', key: 'fake-key'}, opts, function( err, res ){
-          should.exist(err);
-          should.not.exist(res);
-          done();
-        });
-      });
-    });
+      tiles.stashTile(file, data, params, function (err, res) {
+        should.not.exist(err)
+        should.exist(res)
+        done()
+      })
+    })
 
-    describe('vector-tiles', function(){
-      it('when creating a tile', function(done){
-        var file = __dirname + '/fixtures/co.6.13.24.vector.pbf';
+  })
 
-        var params = {
-          format: 'vector.pbf',
-          name: '61324',
-          z: 6,
-          x: 13,
-          y: 24
-        };
+  describe('png-tiles', function () {
+    it('when creating a tile', function (done) {
+      var file = __dirname + '/fixtures/5.5.12.png'
 
-        tiles.stashTile( file, data, params, function( err, res ){
-          should.not.exist(err);
-          should.exist(res);
-          done();
-        });
-      });
+      var params = {
+        format: 'png',
+        name: '61324',
+        z: 5,
+        x: 5,
+        y: 12
+      }
 
-    });
+      tiles.stashTile(file, data, params, function (err, res) {
+        should.not.exist(err)
+        should.exist(res)
+        done()
+      })
+    })
+  })
 
-    describe('png-tiles', function(){
-      it('when creating a tile', function(done){
+  describe('utf-tiles', function () {
+    it('when creating a tile', function (done) {
+      var file = __dirname + '/fixtures/5.5.12.utf'
 
-        var file = __dirname + '/fixtures/5.5.12.png';
+      var params = {
+        format: 'utf',
+        name: '61324',
+        z: 5,
+        x: 5,
+        y: 12
+      }
 
-        var params = {
-          format: 'png',
-          name: '61324',
-          z: 5,
-          x: 5,
-          y: 12
-        };
+      tiles.stashTile(file, data, params, function (err, res) {
+        should.not.exist(err)
+        should.exist(res)
+        done()
+      })
+    })
+  })
 
-        tiles.stashTile( file, data, params, function( err, res ){
-          should.not.exist(err);
-          should.exist(res);
-          done();
-        });
-      });
-    });
-
-    describe('utf-tiles', function(){
-      it('when creating a tile', function(done){
-
-        var file = __dirname + '/fixtures/5.5.12.utf';
-
-        var params = {
-          format: 'utf',
-          name: '61324',
-          z: 5,
-          x: 5,
-          y: 12
-        };
-
-        tiles.stashTile( file, data, params, function( err, res ){
-          should.not.exist(err);
-          should.exist(res);
-          done();
-        });
-      });
-    });
-
-});
-
+})
